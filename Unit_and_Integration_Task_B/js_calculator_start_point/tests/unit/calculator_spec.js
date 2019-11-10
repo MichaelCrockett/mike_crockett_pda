224c1e5f809a40efc53ada0aecf38d04afaab866
Michael Crockett <mike.crockett76@gmail.com>
2019-11-10 11:42:37 +0000

    Unit tests refactored

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -7,41 +7,41 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
+  it('it has a sample test', function() {
     assert.equal(true, true)
   })
 
-  it('should add', function(){
+  it('should add', function() {
     calculator.previousTotal = 4;
     calculator.add(1);
     assert.equal(calculator.runningTotal, 5);
   })
 
-  it('should subtract', function(){
+  it('should subtract', function() {
     calculator.previousTotal = 7;
     calculator.subtract(4);
     assert.equal(calculator.runningTotal, 3);
   })
 
-  it('should multiply', function(){
+  it('should multiply', function() {
     calculator.previousTotal = 3;
     calculator.multiply(5);
     assert.equal(calculator.runningTotal, 15);
   })
 
-  it('should divide', function(){
+  it('should divide', function() {
     calculator.previousTotal = 21;
     calculator.divide(7);
     assert.equal(calculator.runningTotal, 3);
   })
 
-  it('should concatenate multiple button clicks', function(){
+  it('should concatenate multiple button clicks', function() {
     calculator.numberClick(6);
     calculator.numberClick(2);
     assert.equal(calculator.runningTotal, 62);
   })
 
-  it('should chain multiple operations together', function(){
+  it('should chain multiple operations together', function() {
     calculator.numberClick(6);
     calculator.operatorClick('+');
     calculator.numberClick(2);
@@ -52,7 +52,7 @@ describe('calculator', function () {
   })
 
   it('should clear the running total without affecting calculation',
-  function(){
+  function() {
     calculator.numberClick(7);
     calculator.operatorClick('-');
     calculator.numberClick(5);
