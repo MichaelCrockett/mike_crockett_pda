a20a4cf7a64dbe7c64515974fcf3afad2e27888a
Michael Crockett <mike.crockett76@gmail.com>
2019-11-10 11:42:01 +0000

    "Unit tests refactored"

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -12,54 +12,54 @@ describe('calculator', function () {
   })
 
   it('should add', function(){
-   calculator.previousTotal = 4;
-   calculator.add(1);
-   assert.equal(calculator.runningTotal, 5);
- })
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    assert.equal(calculator.runningTotal, 5);
+  })
 
- it('should subtract', function(){
-   calculator.previousTotal = 7;
-   calculator.subtract(4);
-   assert.equal(calculator.runningTotal, 3);
- })
+  it('should subtract', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(calculator.runningTotal, 3);
+  })
 
- it('should multiply', function(){
-     calculator.previousTotal = 3;
-     calculator.multiply(5);
-     assert.equal(calculator.runningTotal, 15);
- })
+  it('should multiply', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, 15);
+  })
 
- it('should divide', function(){
-   calculator.previousTotal = 21;
-   calculator.divide(7);
-   assert.equal(calculator.runningTotal, 3);
- })
+  it('should divide', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(calculator.runningTotal, 3);
+  })
 
- it('should concatenate multiple button clicks', function(){
-   calculator.numberClick(6);
-   calculator.numberClick(2);
-   assert.equal(calculator.runningTotal, 62);
- })
+  it('should concatenate multiple button clicks', function(){
+    calculator.numberClick(6);
+    calculator.numberClick(2);
+    assert.equal(calculator.runningTotal, 62);
+  })
 
- it('should chain multiple operations together', function(){
-   calculator.numberClick(6);
-   calculator.operatorClick('+');
-   calculator.numberClick(2);
-   calculator.operatorClick('-');
-   calculator.numberClick(5);
-   calculator.operatorClick('=');
-   assert.equal(calculator.runningTotal, 3);
- })
+  it('should chain multiple operations together', function(){
+    calculator.numberClick(6);
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.operatorClick('-');
+    calculator.numberClick(5);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 3);
+  })
 
- it('should clear the running total without affecting calculation',
-   function(){
-   calculator.numberClick(7);
-   calculator.operatorClick('-');
-   calculator.numberClick(5);
-   calculator.clearClick();
-   calculator.numberClick(2);
-   calculator.operatorClick('=');
-   assert.equal(calculator.runningTotal, 5);
- })
+  it('should clear the running total without affecting calculation',
+  function(){
+    calculator.numberClick(7);
+    calculator.operatorClick('-');
+    calculator.numberClick(5);
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    assert.equal(calculator.runningTotal, 5);
+  })
 
 });
